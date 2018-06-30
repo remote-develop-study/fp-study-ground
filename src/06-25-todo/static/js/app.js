f1964fe6ed723dbcdf598b9c91435e948ba0a4b0
Chris Yang <ysm0622@gmail.com>
2018-07-01 02:02:28 +0900

    Done: VanillaJS todo list

---
+++ src/06-25-todo/static/js/app.js
@@ -0,0 +1,87 @@
+import $ from './selector.js'
+import state from './state.js'
+
+(() => {
+	const todoDOM = todo => `
+	<li class="${todo.done && 'completed'}">
+		<input class="toggle" type="checkbox" data-id="${todo.id}" ${todo.done && 'checked'}>
+		<label>${todo.title}</label>
+		<button data-id="${todo.id}" class="delete"/>
+	</li>`
+
+	const getCurrentId = () => state.todos.length !== 0 ? Math.max(...state.todos.map(todo => todo.id)) + 1 : 0
+
+	const newTodo = title => ({
+		id: getCurrentId(),
+		title,
+		done: false
+	})
+
+	const render = () => {
+		$('.todo-list').innerHTML =
+			state.todos
+			.filter(todo => state.filter !== 'active' || !todo.done)
+			.filter(todo => state.filter !== 'completed' || todo.done)
+			.reverse()
+			.map(todoDOM)
+			.join('')
+		$('.todo-count').innerHTML = `
+		<strong>${state.todos.filter(todo => !todo.done).length}</strong> items left
+		`
+	}
+
+	const addPropertyReactiveness = () => {
+		Object.defineProperty(state, 'todos', {
+			set(v) {
+				state._todos = v
+				localStorage._todos = JSON.stringify(v)
+				render()
+			},
+			get() {
+				return state._todos
+			}
+		})
+		Object.defineProperty(state, 'filter', {
+			set(v) {
+				state._filter = v
+				render()
+			},
+			get() {
+				return state._filter
+			}
+		})
+	}
+
+	const bindDOMEvents = () => {
+		$('.new-todo').addEventListener('keypress', e => {
+			if (e.keyCode !== 13 || !e.target.value.trim()) return
+			state.todos = [...state.todos, newTodo(e.target.value)]
+			e.target.value = ''
+		})
+		$('ul.filters li a')
+			.map(DOM => DOM.addEventListener('click', e => {
+				state.filter = e.target.id
+				$('#all').classList.remove('selected')
+				$('#active').classList.remove('selected')
+				$('#completed').classList.remove('selected')
+				$(`#${e.target.id}`).classList.add('selected')
+			}))
+		$('.todo-list').addEventListener('click', e => {
+			if (e.target.classList.value === 'delete') {
+				state.todos = state.todos.filter(todo => todo.id != e.target.dataset.id)
+			}
+			if (e.target.classList.value === 'toggle') {
+				state.todos = state.todos.map(todo => todo.id == e.target.dataset.id ? { ...todo,
+					done: !todo.done
+				} : todo)
+			}
+		})
+		$('.clear-completed').addEventListener('click', e => {
+			state.todos = state.todos.filter(todo => !todo.done)
+		})
+	}
+
+	bindDOMEvents()
+	addPropertyReactiveness()
+	state.todos = localStorage._todos ? JSON.parse(localStorage._todos) : []
+})()
