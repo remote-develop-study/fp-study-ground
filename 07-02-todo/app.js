6e42a1dd31769dea3c7ca629f31f9f58438f6b3b
Jeewhan <distinct19@gmail.com>
2018-07-07 22:23:59 +0900

    Init app

---
+++ 07-02-todo/app.js
@@ -0,0 +1,78 @@
+import { $, $$ } from "./tool.js";
+import Todo from "./todo.js";
+
+const TodoApp = (() => {
+  const Private = Symbol();
+
+  return class {
+    constructor(parent) {
+      if (typeof parent != "string" || !parent) throw "invalid param";
+      this[Private] = { parent };
+    }
+
+    init(todos) {
+      Object.assign(this[Private], { todos });
+
+      const app = $(this[Private].parent);
+      if (!app) throw "invalid parent";
+
+      this._newTodoListen();
+      this._render();
+    }
+
+    _save() {
+      localStorage.data = JSON.stringify({ todos: this[Private].todos });
+      this._render();
+    }
+
+    _render() {
+      const { todos } = this[Private];
+      if (!todos.length) return;
+      else {
+        this._mainDisplay();
+        const todo_list = this._create_todo_list();
+        $("#todo-list").innerHTML = todo_list;
+        $("#todo-count").innerHTML = this._todoCount();
+      }
+    }
+
+    _create_todo_list() {
+      return this[Private].todos.map((v, i) => v.templatify(i)).join("");
+    }
+
+    _newTodoListen() {
+      const new_todo = $("#new-todo");
+      new_todo.addEventListener("keypress", ({ key, target: { value } }) => {
+        if (key != "Enter") return;
+        else {
+          this[Private].todos.push(new Todo(value));
+          this._save();
+          new_todo.value = "";
+          this._render();
+        }
+      });
+    }
+
+    _mainDisplay() {
+      const main = $("#main");
+      if (main.style.display == "none") main.style.display = "block";
+    }
+
+    _todoCount() {
+      const count = this[Private].todos.filter(v => v.status).length;
+      return count == 0 || count == 1
+        ? "${count} item left"
+        : "${count} items left";
+    }
+  };
+})();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const todoapp = new TodoApp("#app");
+  const { todos } =
+    typeof localStorage.data == "string" &&
+    Array.isArray(JSON.parse(localStorage.data).todos)
+      ? JSON.parse(localStorage.data)
+      : { todos: [] };
+  todoapp.init(todos);
+});
