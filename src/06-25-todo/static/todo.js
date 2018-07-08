1eb872d2c8b39f5d07fff9631107258e562b0324
rudwnok123 <rudwnok123@naver.com>
2018-07-09 00:44:59 +0900

    로컬스토리지 완료

--- src/06-25-todo/static/todo.js
+++ src/06-25-todo/static/todo.js
@@ -1,14 +1,21 @@
 var $input_value = document.querySelector('.new-todo').value;
 var $input_todo = document.querySelector('.new-todo');
 // 로컬스토리지 저장소
-// 저장되는 값
-// var $value = [];
-var data = [];
+var data = localStorage.data ? JSON.parse(localStorage.data) : []
+var data_local = JSON.parse(window.localStorage.getItem("data"));
+
 
 getItem(data);
+//갯수 보기
+setInterval(count,500);
+function count(){
+  document.querySelector('.todo-count').innerHTML = data.length +' items left';
+  // console.log('aa');
+}
+
 // 엔터 눌렀을시 저장되게
 function list_add() {
-  $input_todo.addEventListener('keypress', function (e) {
+  $input_todo.addEventListener('keyup', function (e) {
     let x = e.which || e.keyCode;
     if (x == 13) {
       //공백일때
@@ -37,9 +44,7 @@ list_add();
 //저장
 function setItem(data) {
   window.localStorage.setItem('data', JSON.stringify(data));
-
   for (var i = 0; i < data.length; i++) {
-    console.log('setItem 함수 내부 ' + data);
     var li = document.createElement('li');
     var text = document.createTextNode(data[i]);
     li.appendChild(text);
@@ -57,7 +62,6 @@ function setItem(data) {
 function getItem(data) {
   var $ul = document.querySelector('.todo-list');
   var $li = document.createElement('li');
-  var data = JSON.parse(window.localStorage.getItem("data"));
 
   // jquery 뿌리기
   // for (var i = 0; i < data.length; i++) {
@@ -71,35 +75,41 @@ function getItem(data) {
   // }
 
   // 바닐라js 뿌리기
-  for (var i = 0; i < data.length; i++) {
+  for (var i = 0; i < data_local.length; i++) {
     var htm = '';
     htm = '<li class="false">' +
       '<input type="checkbox" class="toggle">' +
-      '<label>' + data[i] + '</label>' +
+      '<label>' + data_local[i] + '</label>' +
       '<button class="delete"></button>' +
       '</li>'
     $li.innerHTML = htm;
     $ul.appendChild($li.firstChild);
   }
-  console.log(data);
+  // console.log(data_local);
+  document.querySelector('.todo-count').innerHTML = data_local.length + ' items left';
 };
 
-//data delete
-function a() {
+//삭제
+// var $delete_btn = document.querySelector('.delete');
+// $delete_btn.addEventListener('click', delete_list)
+// function delete_list() {
+// }
 
-}
 
-//완료 토글
-var $checkbox = document.querySelector('.toggle');
 var $mother_chckbox = document.querySelector('.toggle-all');
+//완료
+//.todo-list 에 걸어보기
+document.querySelector('.toggle').addEventListener('click', function (e) {
+  e.target.parentElement.classList.toggle('finished');
+  // this.parentNode.classList.toggle('finished');
+  // this.parentElement.style.display = 'none';
+    // this.parentElement.style.background = 'black';
+    // console.log(this);
+})
 
-$checkbox.addEventListener('click',finish)
-$mother_chckbox.addEventListener('click',all_finish)
+//전체 완료
+$mother_chckbox.addEventListener('click', function(){
 
-function finish() {
-  
-  this.parentNode.classList.toggle('finished');
-}
-function all_finish(){
+})
 
-};
+
