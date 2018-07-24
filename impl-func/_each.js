ae3c17106f59d94c6fa0678827d3070cd6af25ac
rudwnok123 <rudwnok123@naver.com>
2018-07-25 01:53:39 +0900

    each?

--- impl-func/_each.js
+++ impl-func/_each.js
@@ -1,1 +1,12 @@
 // each기능을 구현
+ function each (collection, iterator) {
+  if (Array.isArray(collection)) {
+    for (var i = 0; i < collection.length; i++) {
+      iterator(collection[i], i, collection);
+    }
+  } else {
+    for (var key in collection) {
+      iterator(collection[key], key, collection);
+    }
+  }
+};
--- impl-func/common.js
+++ impl-func/common.js
@@ -1,13 +1,13 @@
 // var $wired = document.getElementsByTagName('wired-item');
 var $combo_item = document.querySelector('wired-combo');
 var number1;
-
+var arr = [2,3,4,5,6,7,8,9]
 
 $combo_item.addEventListener('click',function(e){
   number1 = e.target.value;
   console.log('선택한 숫자 : '+number1);
   //each
-
+  each(arr);
   //filter
 
   //map
