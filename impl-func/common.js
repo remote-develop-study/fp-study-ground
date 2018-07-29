775efcb2f5c8933c88a1940e0af2f5c308951249
rudwnok123 <rudwnok123@naver.com>
2018-07-29 22:27:26 +0900

    merge

--- impl-func/common.js
+++ impl-func/common.js
@@ -1,60 +1,20 @@
 // var $wired = document.getElementsByTagName('wired-item');
 var $combo_item = document.querySelector('wired-combo');
-<<<<<<< Updated upstream
-var number1;
-var arr = [2,3,4,5,6,7,8,9]
-=======
 // var _each = _curryr(_each);
 var start_dan;
 var end_dan = 9;
->>>>>>> Stashed changes
 
 $combo_item.addEventListener('click',function(e){
   start_dan = e.target.value;
   console.log('선택한 숫자 : '+start_dan);
   //each
-<<<<<<< Updated upstream
-  each(arr);
-  iterator(arr);
-
-  
-=======
   // _each(start_dan);
   each(start_dan);
->>>>>>> Stashed changes
   //filter
 
   //map
 })
 
-<<<<<<< Updated upstream
-
-function iterator(arr){
-  var nextIndex = 0;
-  return {
-     next: function(){
-         return nextIndex < array.length ?
-             {value: array[nextIndex++], done: false} :
-             {done: true};
-     }
-  }
-}
-
-
-
-
- function each (collection, iterator) {
-  if (Array.isArray(collection)) {
-    for (var i = 0; i < collection.length; i++) {
-      iterator(collection[i], i, collection);
-    }
-  } else {
-    for (var key in collection) {
-      iterator(collection[key], key, collection);
-    }
-  }
-};
-=======
 // function _curryr(fn) {
 //   return function(a, b) {
 //     return arguments.length == 2 ? fn(a, b) : function(b) { return fn(b, a); };
@@ -83,4 +43,3 @@ function _each(list){
   return keys * end_dan;
 }
 
->>>>>>> Stashed changes
